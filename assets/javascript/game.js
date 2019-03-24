c99142d4ce66ba08f1ab30c475d108d9db18a6d4
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-03-23 20:01:31 -0500

    more progress on logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,8 @@ var nissanSpeed = Math.floor(Math.random() * 11) + 1;
 var lamboSpeed = Math.floor(Math.random() * 11) + 1;
 var amgSpeed = Math.floor(Math.random() * 11) + 1;
 var r8Speed = Math.floor(Math.random() * 11) + 1;
+var score = nissanSpeed + lamboSpeed + amgSpeed + r8Speed; //0
+console.log("Computer score: " + score)
 
 // This is the counter for wins/losses
 var wins = 0;
@@ -21,16 +23,18 @@ console.log("Computer Pick: " + r8Speed);
 
 $("#target-random-number").text(computerPick);
 
+// You are going to add score + carPickedSpeed
+// if score === computerPick -> Win
+// else if score > computerPick -> loose
+// otherwise the game continues
 
-
-
-
-if (nissanSpeed + lamboSpeed + amgSpeed + r8Speed === computerPick) {
-
+if (score === computerPick) {
 wins++;
     document.querySelector("#wins").innerHTML = wins;
-
 }
     
-
+if (score >= computerPick) {
+    losses++;
+        document.querySelector("#losses").innerHTML = wins;
+    }
 
